1b964490c5b94b823711d652b31445a2c08e0ac8
Thierry Pierre Dutoit <33611039+thierryxdp@users.noreply.github.com>
2021-10-01 15:10:12 -0300

    Extracting Emotions from Title + Subtitle and printing only top 2 emotions with percentual.

--- ExtractingEmotions/emotion.py
+++ ExtractingEmotions/emotion.py
@@ -1,40 +1,26 @@
 import paralleldots
+from math import ceil
+from googletrans import Translator
+
+# Creating Translator Objector from Google Translate API
+translator = Translator()
+
 
 # Setting API Key
 api_key = paralleldots.set_api_key("Ax2gN8ym1LLf050wvrgHSzDcPDIfDnUDiOEc0DETpPo")
-
-f = open('C:\\Users\\thier\\ExtractingEmotions\\articles\\juliette_title.txt', 'r', encoding='utf-8')
+    
+f = open('C:\\Users\\thier\\ExtractingEmotions\\articles\\comidas_processadas_all.txt', 'r', encoding='utf-8')
 
 if f.mode == 'r':
     text = f.read()
-
-    print(text)
     
-    print("Emotion\n")
-    print(paralleldots.emotion(text))
+    result = translator.translate(text)
+    
+    print(result.text + "\n")
+    emotions = paralleldots.emotion(result.text)
+    emotions_sorted = sorted(emotions['emotion'].items(), key=lambda x: x[1], reverse=True)
+    print(str(emotions_sorted[0][0]) + ("(") + str(ceil(emotions_sorted[0][1]*100)) + "%) and " + str(emotions_sorted[1][0]) + ("(") + str(ceil(emotions_sorted[1][1]*100)) +"%)\n")
+    
 else:
     print("Nao foi possivel abrir o arquivo :( ")
-
-f = open('C:\\Users\\thier\\ExtractingEmotions\\articles\\educacao_title.txt', 'r', encoding='utf-8')
-
-if f.mode == 'r':
-    text = f.read()
-
-    print(text)
     
-    print("Emotion\n" + "\n")
-    print(paralleldots.emotion(text))
-else:
-    print("Nao foi possivel abrir o arquivo :( ")
-
-f = open('C:\\Users\\thier\\ExtractingEmotions\\articles\\judicial_title.txt', 'r', encoding='utf-8')
-
-if f.mode == 'r':
-    text = f.read()
-
-    print(text)
-    
-    print("Emotion\n")
-    print(paralleldots.emotion(text))
-else:
-    print("Nao foi possivel abrir o arquivo :( ")
